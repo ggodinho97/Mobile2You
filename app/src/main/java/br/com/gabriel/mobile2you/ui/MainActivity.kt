fb9831b2733637a1b716b4323f112678777a3327
Perkles <gabriel.godinho2016@gmail.com>
2021-08-19 20:51:21 -0300

    Criada a parte da visualização com a imagem e informações do filme

--- app/src/main/java/br/com/gabriel/mobile2you/ui/MainActivity.kt
+++ app/src/main/java/br/com/gabriel/mobile2you/ui/MainActivity.kt
@@ -6,7 +6,9 @@ import android.widget.Toast
 import br.com.gabriel.mobile2you.R
 import br.com.gabriel.mobile2you.model.Movie
 import br.com.gabriel.mobile2you.model.SimilarMovie
+import br.com.gabriel.mobile2you.utils.Constants
 import br.com.gabriel.mobile2you.viewmodel.MovieViewModel
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import retrofit2.Call
@@ -16,16 +18,47 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MovieViewModel by viewModel()
+    private var isLiked = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvany.setOnClickListener {
+        setupView()
 
+    }
+
+    private fun setupView() {
+        setupListenears()
+        getMovie()
+    }
+
+    private fun setupListenears() {
+        imgLike.setOnClickListener {
+            isLiked = if (isLiked) {
+                imgLike.setImageResource(R.drawable.heart_outline)
+                false
+            }else {
+                imgLike.setImageResource(R.drawable.cards_heart)
+                true
+            }
+        }
+    }
+
+    private fun getMovie(){
             viewModel.getMovie(object : Callback<Movie> {
                 override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
-                    Toast.makeText(this@MainActivity, "Sucesso Filme!", Toast.LENGTH_SHORT).show()
+                    val movie = response.body()
+
+                    Glide
+                        .with(this@MainActivity)
+                        .load(Constants.BASE_IMAGE_URL + movie?.poster_path )
+                        .into(imgBanner)
+
+                    tvTitle.text = movie?.title
+                    tvLikes.text = getString(R.string.voteCounts, movie?.vote_count)
+                    tvViews.text = getString(R.string.countViews, movie?.popularity)
+
                 }
 
                 override fun onFailure(call: Call<Movie>, t: Throwable) {
@@ -54,4 +87,3 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-}
